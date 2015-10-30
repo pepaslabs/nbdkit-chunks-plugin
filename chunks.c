9325be3c123ddd6a2e2ed7edb836d31152c8f33f
Jason Pepas <jasonpepas@gmail.com>
2015-10-29 23:45:33 -0500

    starting work on parsing key value options

--- chunks.c
+++ chunks.c
@@ -9,13 +9,38 @@
 #include <config.h>
 #include <nbdkit-plugin.h>
 
+#include <string.h> // strcmp()
+
+
 #define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS
+
+
+int chunks_config(const char *key, const char *value)
+{
+	if (strcmp(key, "dir") == 0)
+	{
+		char *chunks_dir = nbdkit_absolute_path(value);
+		if (chunks_dir == NULL)
+		{
+			nbdkit_error("nbdkit_absolute_path() failed on dir '%s'", value);
+			return -1;
+		}
+
+		return 0;
+	}
+	else
+	{
+		nbdkit_error("Unrecognized parameter: '%s'", key);
+		return -1;
+	}
+}
 
 static struct nbdkit_plugin plugin = {
   .name              = "chunks",
   .longname          = "nbdkit chunks plugin",
   .description       = "An nbdkit plugin which stores data in many small files, rather than one large backing file.",
   .version           = "0.0",
+  .config            = chunks_config
 };
 
 NBDKIT_REGISTER_PLUGIN(plugin)
