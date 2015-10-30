5b561eef24cf427a063ea57e4897f881716a2a12
Jason Pepas <jasonpepas@gmail.com>
2015-10-29 23:35:12 -0500

    adding .description and setting initial version to 0.0

--- chunks.c
+++ chunks.c
@@ -4,6 +4,8 @@
  * Released under the terms of the MIT license.  See https://opensource.org/licenses/MIT
  */
 
+// see https://github.com/libguestfs/nbdkit/blob/master/docs/nbdkit-plugin.pod
+
 #include <config.h>
 #include <nbdkit-plugin.h>
 
@@ -12,7 +14,8 @@
 static struct nbdkit_plugin plugin = {
   .name              = "chunks",
   .longname          = "nbdkit chunks plugin",
-  .version           = PACKAGE_VERSION,
+  .description       = "An nbdkit plugin which stores data in many small files, rather than one large backing file.",
+  .version           = "0.0",
 };
 
 NBDKIT_REGISTER_PLUGIN(plugin)
