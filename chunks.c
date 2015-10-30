3a1776b3e78f4e5af215b346f9f5dfc0385c1226
Jason Pepas <jasonpepas@gmail.com>
2015-10-29 23:08:06 -0500

    fixing compilation issue

--- chunks.c
+++ chunks.c
@@ -4,6 +4,11 @@
  * Released under the terms of the MIT license.  See https://opensource.org/licenses/MIT
  */
 
+#include <config.h>
+#include <nbdkit-plugin.h>
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_REQUESTS
+
 static struct nbdkit_plugin plugin = {
   .name              = "chunks",
   .longname          = "nbdkit chunks plugin",
