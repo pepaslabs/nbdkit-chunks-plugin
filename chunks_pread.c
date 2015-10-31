05da1caf5112a63675f26a32cf847719554d36e4
Jason Pepas <jasonpepas@gmail.com>
2015-10-31 05:48:23 -0500

    pulling in hexify.  working on chunks_pread().

--- chunks_pread.c
+++ chunks_pread.c
@@ -9,15 +9,23 @@
 #include "chunks_pread.h"
 
 #include "chunks_dev_t.h"
+#include "hexify.h"
 
 #include <nbdkit-plugin.h> // nbdkit_error(), etc.
 #include <stdio.h> // fopen(), etc.
+#include <string.h> // strlen(), etc.
 
 extern chunks_dev_t dev;
 
-int _read_from_open_file(FILE *fp, char *filepath, uint32_t src_count, void *dest)
+int _read_from_open_file(FILE *fp, char *filepath, uint64_t offset, uint32_t count, unsigned char *dest)
 {
-    size_t bytes_remaining = src_count;
+    if (fseek(fp, offset, SEEK_SET) != 0)
+    {
+        nbdkit_error("Unable to fseek '%s'", filepath);
+        return -1;
+    }
+
+    size_t bytes_remaining = count;
 
     while(bytes_remaining > 0)
     {
@@ -40,7 +48,7 @@ int _read_from_open_file(FILE *fp, char *filepath, uint32_t src_count, void *des
     return 0;
 }
 
-int _read_from_chunk_at_path(char *chunk_path, uint64_t src_offset, uint32_t src_count, void *dest, uint64_t dest_offset)
+int _read_from_chunk_at_path(char *chunk_path, uint64_t offset, uint32_t count, unsigned char *dest)
 {
     FILE *fp = fopen(chunk_path, "r");
     if (fp == NULL)
@@ -49,7 +57,7 @@ int _read_from_chunk_at_path(char *chunk_path, uint64_t src_offset, uint32_t src
         return -1;
     }
 
-    int ret = _read_from_open_file(fp, chunk_path, src_count, dest);
+    int ret = _read_from_open_file(fp, chunk_path, offset, count, dest);
 
     if (fclose(fp) != 0)
     {
@@ -60,27 +68,40 @@ int _read_from_chunk_at_path(char *chunk_path, uint64_t src_offset, uint32_t src
     return ret;
 }
 
-int _chunk_path(uint64_t chunk_num)
+void _uint64_t_to_hex(uint64_t in, char *buf, size_t buf_size)
 {
-    // dir/chunks/chunk_num
-
-    // FIXME
+    hexify((unsigned char*)(&in), sizeof(in), buf, buf_size);
 }
 
-int _read_from_chunk_num(uint64_t chunk_num, uint64_t offset, uint32_t count, void *buf)
+int _read_from_chunk_num(uint64_t chunk_num, uint64_t offset, uint32_t count, unsigned char *dest)
 {
-    // FIXME
+    size_t buff_size = strlen(dev.dir_path) + strlen("/chunks/ffffffffffffffff") + 1;
+    char chunk_path[buff_size];
+    int chars_written = snprintf(chunk_path, buff_size, "%s/chunks/", dev.dir_path);
+    _uint64_t_to_hex(chunk_num, chunk_path+chars_written, buff_size-chars_written);
+
+    return _read_from_chunk_at_path(chunk_path, offset, count, dest);
 }
 
 int chunks_pread(void *passed_handle, void *buf, uint32_t count, uint64_t offset)
 {
-    return -1;
+    while(count > 0)
+    {
+        uint64_t chunk_num = offset << dev.chunk_shift;
+        uint64_t chunk_offset = offset % dev.chunk_size;
+        uint64_t chunk_remaining = dev.chunk_size - chunk_offset;
+        uint32_t chunk_count = count <= chunk_remaining ? count : chunk_remaining;
 
-    // split the read up into chunks
-    // read each chunk
+        int ret = _read_from_chunk_num(chunk_num, chunk_offset, chunk_count, (unsigned char*)buf);
+        if (ret != 0)
+        {
+            return ret;
+        }
 
-    uint64_t first_chunk_number = offset << dev.chunk_shift;
-    uint64_t first_chunk_offset = offset % dev.chunk_size;
+        offset += chunk_count;
+        count -= chunk_count;
+        buf += chunk_count;
+    }
 
-    // FIXME
+    return 0;
 }
