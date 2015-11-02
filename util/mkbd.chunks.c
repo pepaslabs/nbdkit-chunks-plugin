d4397ca76feb8de799534f15d82e97fadaa377df
Jason Pepas <jasonpepas@gmail.com>
2015-11-01 22:04:34 -0600

    implemented return codes

--- util/mkbd.chunks.c
+++ util/mkbd.chunks.c
@@ -8,6 +8,28 @@
 #include "args_t.h"
 #include "metadata_t.h"
 
+enum {
+    ERROR_parse_size_str_NO_LEADING_NUMERIC_CHARS = -1,
+    ERROR_parse_size_str_WOULD_OVERFLOW_UINT64 = -2,
+    ERROR_parse_suffix_BAD_in_size = -3,
+
+    ERROR_populate_metadata_from_args_SIZE_OFFSET = -10,
+    ERROR_populate_metadata_from_args_SIZE_NO_LEADING_NUMERIC_CHARS = -11,
+    ERROR_populate_metadata_from_args_SIZE_WOULD_OVERFLOW_UINT64 = -12,
+    ERROR_populate_metadata_from_args_SIZE_BAD_in_size = -13,
+
+    ERROR_populate_metadata_from_args_CHUNK_SIZE_OFFSET = -20,
+    ERROR_populate_metadata_from_args_CHUNK_SIZE_NO_LEADING_NUMERIC_CHARS = -21,
+    ERROR_populate_metadata_from_args_CHUNK_SIZE_WOULD_OVERFLOW_UINT64 = -22,
+    ERROR_populate_metadata_from_args_CHUNK_SIZE_BAD_in_size = -23
+};
+
+enum _suffix_t { NONE=0, K, KB, M, MB, G, GB, T, TB };
+typedef enum _suffix_t suffix_t;
+
+args_t args;
+metadata_t metadata;
+
 // copy in to out, stopping at the first non-numeric character.
 // returns the number of charaters copied, or -1.
 // in_size is the length of 'in' plus the terminating '\0'.
@@ -52,8 +74,7 @@ int copy_numeric(char *in, size_t in_size, char *out, size_t out_size)
     return i;
 }
 
-enum _suffix_t { NONE=0, K, KB, M, MB, G, GB, T, TB };
-typedef enum _suffix_t suffix_t;
+#define RETURN_SUCCESS 0
 
 // translate a suffix string into a suffix_t enum.
 // in_size is the length of 'in' plus the terminating '\0'.
@@ -80,10 +101,10 @@ int parse_suffix(char *in, size_t in_size, suffix_t *out)
     }
     else
     {
-        return -1;
+        return ERROR_parse_suffix_BAD_in_size;
     }
 
-    return 0;
+    return RETURN_SUCCESS;
 }
 
 uint64_t suffix_to_multiplier(suffix_t *suffix)
@@ -105,11 +126,13 @@ uint64_t suffix_to_multiplier(suffix_t *suffix)
 // in_size is the length of 'in' plus the terminating '\0'.
 int parse_size_str(char *in, size_t in_size, uint64_t *out)
 {
+    int ok;
+
     char int_buf[sizeof("18446744073709551615")];
     int chars_copied = copy_numeric(in, in_size, int_buf, sizeof(int_buf));
     if (chars_copied == 0)
     {
-        return -1;
+        return ERROR_parse_size_str_NO_LEADING_NUMERIC_CHARS;
     }
 
     int base = 10;
@@ -118,54 +141,64 @@ int parse_size_str(char *in, size_t in_size, uint64_t *out)
     char *suffix_in = in + chars_copied;
     size_t suffix_size = in_size - chars_copied;
     suffix_t suffix;
-    if (parse_suffix(suffix_in, suffix_size, &suffix) == -1)
-    {
-        return -1;
-    }
+    ok = parse_suffix(suffix_in, suffix_size, &suffix);
+    if (ok < 0) return ok;
 
     uint64_t multiplier = suffix_to_multiplier(&suffix);
 
     bool would_overflow = (multiplier > (UINT64_MAX / size));
     if (would_overflow)
     {
-        return -1;
+        return ERROR_parse_size_str_WOULD_OVERFLOW_UINT64;
     }
 
     *out = size * multiplier;
+
+    return RETURN_SUCCESS;
 }
 
-void populate_metadata_from_args()
+void metadata_init_v0(metadata_t *inout)
 {
-    
+    inout->v0.magic = CHUNKS_METADATA_MAGIC;
+    inout->v0.metadata_version = 1;
 }
 
-args_t args;
-metadata_t metadata;
+int populate_metadata_from_args()
+{
+    int ok;
+
+    metadata_init_v0(&metadata);
+
+    uint64_t size;
+    ok = parse_size_str(args.size_str, strlen(args.size_str)+1, &size);
+    if (ok < 0) return ok + ERROR_populate_metadata_from_args_SIZE_OFFSET;
+
+    metadata.v1.dev_size = size;
+
+    uint64_t chunk_size;
+    ok = parse_size_str(args.chunk_size_str, strlen(args.chunk_size_str)+1, &chunk_size);
+    if (ok < 0) return ok + ERROR_populate_metadata_from_args_CHUNK_SIZE_OFFSET;
+
+    metadata.v1.chunk_size = chunk_size;
+
+    return RETURN_SUCCESS;
+}
 
 int main(int argc, char *argv[])
 {
+    int ok;
+
     parse_args(argc, argv);
 
     printf("args.directory: %s\n", args.directory);
     printf("args.size_str: %s\n", args.size_str);
     printf("args.chunk_size_str: %s\n", args.chunk_size_str);
 
-    uint64_t size;
-    int ok = parse_size_str(args.size_str, strlen(args.size_str)+1, &size);
-    if (ok == -1)
-    {
-        return EXIT_FAILURE;
-    }
+    ok = populate_metadata_from_args();
+    if (ok < 0) return ok * -1;
 
-    uint64_t chunk_size;
-    ok = parse_size_str(args.chunk_size_str, strlen(args.chunk_size_str)+1, &chunk_size);
-    if (ok == -1)
-    {
-        return EXIT_FAILURE;
-    }
-
-    printf("size: %llu\n", size);
-    printf("chunk_size: %llu\n", chunk_size);
+    printf("size: %llu\n", metadata.v1.dev_size);
+    printf("chunk_size: %llu\n", metadata.v1.chunk_size);
 
     return EXIT_SUCCESS;
 }
