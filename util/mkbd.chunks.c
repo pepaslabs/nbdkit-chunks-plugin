f83bea0d03960e1063cf0da1c154326888bcb78c
Jason Pepas <jasonpepas@gmail.com>
2015-11-01 20:38:34 -0600

    implemented size parsing

--- util/mkbd.chunks.c
+++ util/mkbd.chunks.c
@@ -8,11 +8,9 @@
 #include "args_t.h"
 #include "metadata_t.h"
 
-args_t args;
-metadata_t metadata;
-
 // copy in to out, stopping at the first non-numeric character.
 // returns the number of charaters copied, or -1.
+// in_size is the length of 'in' plus the terminating '\0'.
 int copy_numeric(char *in, size_t in_size, char *out, size_t out_size)
 {
     if (in_size == 0 || out_size == 0) return 0;
@@ -58,21 +56,22 @@ enum _suffix_t { NONE=0, K, KB, M, MB, G, GB, T, TB };
 typedef enum _suffix_t suffix_t;
 
 // translate a suffix string into a suffix_t enum.
+// in_size is the length of 'in' plus the terminating '\0'.
 // returns 0 on success, -1 on error.
 int parse_suffix(char *in, size_t in_size, suffix_t *out)
 {
-    if (in_size == 0)
+    if (in_size == 0+1)
     {
         *out = NONE;
     }
-    else if (in_size == 1)
+    else if (in_size == 1+1)
     {
         if (strncasecmp(in, "k", 1) == 0) *out = K;
         else if (strncasecmp(in, "m", 1) == 0) *out = M;
         else if (strncasecmp(in, "g", 1) == 0) *out = G;
         else if (strncasecmp(in, "t", 1) == 0) *out = T;
     }
-    else if (in_size == 2)
+    else if (in_size == 2+1)
     {
         if (strncasecmp(in, "kb", 2) == 0) *out = KB;
         else if (strncasecmp(in, "mb", 2) == 0) *out = MB;
@@ -91,18 +90,19 @@ uint64_t suffix_to_multiplier(suffix_t *suffix)
 {
     switch (*suffix)
     {
-        case K:  return 1000;
-        case KB: return 1024;
-        case M:  return 1000*1000;
-        case MB: return 1024*1024;
-        case G:  return 1000*1000*1000;
-        case GB: return 1024*1024*1024;
-        case T:  return (long)1000*1000*1000*1000;
-        case TB: return (long)1024*1024*1024*1024;
+        case KB: return 1000;
+        case K:  return 1024;
+        case MB: return 1000*1000;
+        case M:  return 1024*1024;
+        case GB: return 1000*1000*1000;
+        case G:  return 1024*1024*1024;
+        case TB: return (uint64_t)1000*1000*1000*1000;
+        case T:  return (uint64_t)1024*1024*1024*1024;
         default: return 1;
     }
 }
 
+// in_size is the length of 'in' plus the terminating '\0'.
 int parse_size_str(char *in, size_t in_size, uint64_t *out)
 {
     char int_buf[sizeof("18446744073709551615")];
@@ -117,15 +117,20 @@ int parse_size_str(char *in, size_t in_size, uint64_t *out)
 
     char *suffix_in = in + chars_copied;
     size_t suffix_size = in_size - chars_copied;
-
     suffix_t suffix;
-    char suffix_buf[sizeof("XX")];
-    if (parse_suffix(suffix_buf, sizeof(suffix_buf), &suffix) == -1)
+    if (parse_suffix(suffix_in, suffix_size, &suffix) == -1)
     {
         return -1;
     }
 
     uint64_t multiplier = suffix_to_multiplier(&suffix);
+
+    bool would_overflow = (multiplier > (UINT64_MAX / size));
+    if (would_overflow)
+    {
+        return -1;
+    }
+
     *out = size * multiplier;
 }
 
@@ -134,13 +139,24 @@ void populate_metadata_from_args()
     
 }
 
+args_t args;
+metadata_t metadata;
+
 int main(int argc, char *argv[])
 {
     parse_args(argc, argv);
 
-    printf("directory: %s\n", args.directory);
-    printf("size: %s\n", args.size_str);
-    printf("chunk_size: %s\n", args.chunk_size_str);
+    printf("args.directory: %s\n", args.directory);
+    printf("args.size_str: %s\n", args.size_str);
+    printf("args.chunk_size_str: %s\n", args.chunk_size_str);
 
-    return EXIT_FAILURE;
+    uint64_t size;
+    int ok = parse_size_str(args.size_str, strlen(args.size_str)+1, &size);
+    if (ok == -1)
+    {
+        return EXIT_FAILURE;
+    }
+    printf("size: %llu\n", size);
+
+    return EXIT_SUCCESS;
 }
