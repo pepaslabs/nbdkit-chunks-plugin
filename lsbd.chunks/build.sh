e3e246cd7e006f4c945aff51b5d6e218b2e634c7
Jason Pepas <jasonpepas@gmail.com>
2015-11-02 22:33:57 -0600

    starting work on lsbd.chunks

---
+++ lsbd.chunks/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+for c in *.c
+do
+    o=$( basename "${c}" .c )
+    gcc -std=gnu99 -I.. -o ${o}.o -c ${c}
+done
+
+gcc -std=gnu99 -o lsbd.chunks *.o
---
+++ lsbd.chunks/lsbd.chunks.c
@@ -0,0 +1,6 @@
+#include <stdlib.h> // EXIT_FAILURE, etc.
+
+int main(int argc, char *argv[])
+{
+	return EXIT_FAILURE;
+}
