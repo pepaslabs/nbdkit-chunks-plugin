be73898300043080463b3c0896e1a4ae3aedfdd7
Jason Pepas <jasonpepas@gmail.com>
2015-11-05 23:33:11 -0600

    variable naming

--- chunks_pread.c
+++ chunks_pread.c
@@ -62,11 +62,11 @@ int _read_from_chunk_at_path(char *chunk_path, uint64_t offset, uint32_t count, 
     return ret;
 }
 
-int _read_from_chunk_num(uint64_t chunk_num, uint64_t offset, uint32_t count, unsigned char *dest)
+int _read_from_chunk_num(uint64_t chunk_index, uint64_t offset, uint32_t count, unsigned char *dest)
 {
     size_t buff_size = strlen(dev.dir_path) + strlen("/chunks/18446744073709551615") + 1;
     char chunk_path[buff_size];
-    int chars_written = snprintf(chunk_path, buff_size, "%s/chunks/%0.20llu", dev.dir_path, chunk_num);
+    int chars_written = snprintf(chunk_path, buff_size, "%s/chunks/%0.20llu", dev.dir_path, chunk_index);
 
     return _read_from_chunk_at_path(chunk_path, offset, count, dest);
 }
@@ -75,12 +75,12 @@ int chunks_pread(void *passed_handle, void *buf, uint32_t count, uint64_t offset
 {
     while(count > 0)
     {
-        uint64_t chunk_num = offset >> dev.chunk_shift;
+        uint64_t chunk_index = offset >> dev.chunk_shift;
         uint64_t chunk_offset = offset % dev.chunk_size;
         uint64_t chunk_remaining = dev.chunk_size - chunk_offset;
         uint32_t chunk_count = count <= chunk_remaining ? count : chunk_remaining;
 
-        int ret = _read_from_chunk_num(chunk_num, chunk_offset, chunk_count, (unsigned char*)buf);
+        int ret = _read_from_chunk_num(chunk_index, chunk_offset, chunk_count, (unsigned char*)buf);
         if (ret != 0)
         {
             return ret;
