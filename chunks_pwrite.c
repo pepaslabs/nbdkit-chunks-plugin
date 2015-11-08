083ef7fc3fe35fee00c518551e15051663a21e5e
Jason Pepas <jasonpepas@gmail.com>
2015-11-07 20:48:04 -0600

    if fallocate fails, try ftruncate.  if that fails, just file the file with zeros

--- chunks_pwrite.c
+++ chunks_pwrite.c
@@ -21,6 +21,7 @@
 #include <string.h> // strlen(), etc.
 #include <strings.h> // bzero(), etc.
 #include <errno.h> // ENOENT, etc.
+#include <stdbool.h> // bool, etc.
 
 extern chunks_dev_t dev;
 
@@ -48,8 +49,118 @@ int _write_to_open_file(int fd, char *filepath, uint64_t offset, uint32_t count,
     return 0;
 }
 
+static unsigned char zeros4k[4096] = {0};
+
+int _reserve_new_chunk_manually(int fd, char *filepath, uint64_t size)
+{
+    uint64_t offset = 0;
+    uint64_t count = size;
+    while(count > 0)
+    {
+        int bytes_to_write = count > sizeof(zeros4k) ? sizeof(zeros4k) : count;
+        ssize_t bytes_written = pwrite(fd, zeros4k, bytes_to_write, offset);
+        if (bytes_written < 1)
+        {
+            break;
+        }
+
+        count -= bytes_written;
+        offset += bytes_written;
+    }
+
+    if (count > 0)
+    {
+        nbdkit_error("Unable to pwrite() %llu zeros to '%s': %m", size, filepath);
+        return -1;
+    }
+
+    return 0;
+}
+
+static bool should_try_fallocate = true;
+static bool should_try_ftruncate = true;
+
+int _reserve_new_chunk(int fd, char *filepath, uint64_t size)
+{
+    int err;
+
+    // first, try fallocate()
+    if (should_try_fallocate)
+    {
+        err = fallocate(fd, 0, 0, size);
+        switch (err)
+        {
+            case 0:
+                return 0;
+
+            case EOPNOTSUPP:
+                // fallocate isn't supported, don't bother trying next time.
+                should_try_fallocate = false;
+                break;
+
+            default:
+                nbdkit_error("Unable to fallocate(fd, 0, 0, %llu) for '%s': %m", size, filepath);
+                break;
+        }
+    }
+
+    // if that didn't work try ftruncate()
+    if (should_try_ftruncate)
+    {
+        err = ftruncate(fd, size);
+        switch (err)
+        {
+            case 0:
+                // if fallocate failed but ftrucate succeeded, don't bother trying fallocate next time.
+                should_try_fallocate = false;
+                return 0;
+
+            case EPERM:
+                // ftruncate isn't supported, don't bother trying next time.
+                should_try_ftruncate = false;
+                break;
+
+            default:
+                nbdkit_error("Unable to ftruncate(fd, %llu) for '%s': %m", size, filepath);
+                break;
+        }
+    }
+
+    // as a last restort, try manually filling the file with zeros.
+    err = _reserve_new_chunk_manually(fd, filepath, size);
+    if (err == -1) return -1;
+
+    // if ftruncate failed but the manual method succeeded, don't bother trying ftruncate next time.
+    should_try_ftruncate = false;
+
+    return 0;
+}
+
 #define CREATE_FAIL_IF_EXISTS (O_CREAT|O_EXCL)
 #define CHMOD_RW_______ (S_IRUSR|S_IWUSR)
+
+int _create_chunk_at_path(char *chunk_path)
+{
+    int fd = open(chunk_path, O_WRONLY|CREATE_FAIL_IF_EXISTS, CHMOD_RW_______);
+    if (fd == -1)
+    {
+        nbdkit_error("Unable to create '%s': %m", chunk_path);
+        return -1;
+    }
+
+    int err = _reserve_new_chunk(fd, chunk_path, dev.chunk_size);
+    if (err == -1)
+    {
+        if (close(fd) == -1)
+        {
+            nbdkit_error("Unable to close '%s': %m", chunk_path);
+        }
+
+        return -1;
+    }
+
+    return fd;
+}
 
 int _write_to_chunk_at_path(char *chunk_path, uint64_t offset, uint32_t count, const unsigned char *src)
 {
@@ -58,23 +169,9 @@ int _write_to_chunk_at_path(char *chunk_path, uint64_t offset, uint32_t count, c
     {
         if (errno == ENOENT)
         {
-            fd = open(chunk_path, O_WRONLY|CREATE_FAIL_IF_EXISTS, CHMOD_RW_______);
+            fd = _create_chunk_at_path(chunk_path);
             if (fd == -1)
             {
-                nbdkit_error("Unable to create '%s': %m", chunk_path);
-                return -1;
-            }
-
-            int err = fallocate(fd, 0, 0, dev.chunk_size);
-            if (err == -1)
-            {
-                nbdkit_error("Unable to fallocate '%s': %m", chunk_path);
-
-                if (close(fd) == -1)
-                {
-                    nbdkit_error("Unable to close '%s': %m", chunk_path);
-                }
-
                 return -1;
             }
         }
