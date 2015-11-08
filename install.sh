bf0f1ce3eb6eb3a155a2e9bbe9c4b04d9f9cb9f4
Jason Pepas <jasonpepas@gmail.com>
2015-11-08 10:24:24 +0000

    updating install script

--- install.sh
+++ install.sh
@@ -4,5 +4,16 @@ set -e
 
 name=chunks
 
-sudo cp nbdkit-${name}-plugin.so /usr/lib/x86_64-linux-gnu/nbdkit/plugins/
-sudo chown root:root /usr/lib/x86_64-linux-gnu/nbdkit/plugins/nbdkit-${name}-plugin.so
+if [ -d /usr/lib/arm-linux-gnueabihf/nbdkit/plugins ]
+then
+    dest=/usr/lib/arm-linux-gnueabihf/nbdkit/plugins
+elif [ -d /usr/lib/x86_64-linux-gnu/nbdkit/plugins ]
+then
+    dest=/usr/lib/x86_64-linux-gnu/nbdkit/plugins
+else
+    echo "nowhere to install to." >&2
+    exit 1
+fi
+
+sudo cp nbdkit-${name}-plugin.so ${dest}/
+sudo chown root:root ${dest}/nbdkit-${name}-plugin.so
